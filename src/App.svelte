eb5b73c40d20ace8d2dc72b728d757a77603d03e
Huakun Shen <huakun.shen@huakunshen.com>
2022-01-15 00:32:59 -0500

    Send Command on mount

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,18 @@
   import AutoModeCheckbox from './lib/AutoModeCheckbox.svelte';
   import { onMount } from 'svelte';
 
+  const sendCmd = (cmd) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { cmd });
+    });
+  };
+
   onMount(() => {
     console.log('Password Peeper Mounted');
+    chrome?.storage?.sync?.get(['autoMode', 'mode'], (result) => {
+      console.log(`autoMode init: ${result.autoMode}`);
+      sendCmd(result.mode);
+    });
   });
 </script>
 
--- src/lib/AutoModeCheckbox.svelte
+++ src/lib/AutoModeCheckbox.svelte
@@ -6,17 +6,10 @@
 
   let autoMode = false;
 
-  const sendCmd = (cmd) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { cmd });
-    });
-  };
-
   onMount(() => {
     chrome?.storage?.sync?.get(['autoMode', 'mode'], (result) => {
       console.log(`autoMode init: ${result.autoMode}`);
       autoMode = result.autoMode == null ? false : result.autoMode;
-      if (result.autoMode) sendCmd(result.mode);
     });
   });
 
