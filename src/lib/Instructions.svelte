6e928fd9ac8b623952e45a013a9240bc6ce60331
Huakun Shen <huakun.shen@huakunshen.com>
2022-01-25 08:30:36 -0500

    Update instructions in popup page.

--- src/lib/Instructions.svelte
+++ src/lib/Instructions.svelte
@@ -1,4 +1,5 @@
 <main>
+  <h3>Functionalities</h3>
   <p>The radio buttons are responsible for when how password will be shown. There are 2 options.</p>
   <ol>
     <li>Show password when on hover (mouse over)</li>
@@ -16,6 +17,15 @@
       clicking the password field. A notification will be sent if successful.
     </li>
   </ol>
+  <h3>Keyboard Shortcuts</h3>
+  <p>This extension provides 2 keyboard extensions</p>
+  <ol>
+    <li><code>Ctrl+Shift+L</code>: Tigger the extension popup</li>
+    <li><code>Ctrl+Shift+S</code>: Toggle password on web page without opening extension popup</li>
+  </ol>
+  <p>The keyboard shortcuts should work on Linux, MacOS, Windows, ChromeOS.</p>
+  <p>On MacOS, replace the <code>Ctrl</code> key with <code>Command</code> key.</p>
+  <p>Go to <code>chrome://extensions</code> to change the default keyboard shortcuts.</p>
 </main>
 
 <style>
